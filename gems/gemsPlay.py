2ad321011850dd577f93cd33705ac93a01c86732
Azerxim <mallet.maxime@hotmail.fr>
2020-01-30 15:41:55 +0100

    Banque

--- gems/gemsPlay.py
+++ gems/gemsPlay.py
@@ -41,7 +41,7 @@ def daily(param):
         if DailyMult % 30 == 0:
             m = (DailyMult//30)*5
             sql.addSpinelles(PlayerID, m)
-            desc += "\nBravo pour c'est {0} jours consécutifs :confetti_ball:! Tu as mérité {1}<:spinelle:{2}>`spinelles`".format(DailyMult, m, GF.get_idmoji("spinelle"))
+            desc += "\nBravo pour c'est {0} jours consécutifs :confetti_ball:! Tu as mérité {1}<:spinelle:{2}>`spinelles`".format(DailyMult, m, "{idmoji[spinelle]}")
 
     elif DailyTime == str(jour):
         desc = "Tu as déja reçu ta récompense journalière aujourd'hui. Reviens demain pour gagner plus de :gem:`gems`"
@@ -55,154 +55,164 @@ def daily(param):
     return msg
 
 
-# @commands.command(pass_context=True)
-# async def bank(self, ctx, ARG = None, ARG2 = None):
-#     """Compte épargne"""
-#     #=======================================================================
-#     # Initialistation des variables générales de la fonction
-#     #=======================================================================
-#     ID = ctx.author.id
-#     jour = dt.date.today()
-#     if ARG != None:
-#         mARG = ARG.lower()
-#     else:
-#         mARG = "bal"
-#     for c in GF.objetOutil:
-#         if c.type == "bank":
-#             Taille = c.poids
-#     msg = ""
-#     solde = sql.valueAt(ID, "Solde", "bank")
-#     if solde == 0:
-#         sql.add(ID, "SoldeMax", Taille, "bank")
-#     #=======================================================================
-#     # Affiche le menu principal de la banque
-#     # !bank bal <nom d'un joueur> permet de visualiser l'état de la banque de ce joueur
-#     #=======================================================================
-#     if mARG == "bal":
-#         if sql.spam(ID,GF.couldown_4s, "bank_bal", "gems"):
-#             if ARG2 != None:
-#                 ID = sql.nom_ID(ARG2)
-#                 nom = ctx.guild.get_member(ID)
-#                 ARG2 = nom.name
-#                 title = "Compte épargne de {}".format(ARG2)
-#             else:
-#                 title = "Compte épargne de {}".format(ctx.author.name)
-#             solde = sql.valueAtNumber(ID, "Solde", "bank")
-#             soldeMax = sql.valueAtNumber(ID, "SoldeMax", "bank")
-#             if soldeMax == 0:
-#                 soldeMax = Taille
-#             msg = discord.Embed(title = title,color= 13752280, description = "")
-#             desc = "{} / {} :gem:`gems`\n".format(solde, soldeMax)
-#             msg.add_field(name="Balance", value=desc, inline=False)
-#
-#             desc = "bank **bal** *[name]* | Permet de connaitre la balance d'un utilisateur"
-#             desc += "\nbank **add** *[+/- nombre]* | Permet d'ajouter ou d'enlever des :gem:`gems` de son compte épargne"
-#             desc += "\nbank **saving** | Permet de calculer son épargne (utilisable toute les 4h)"
-#             desc += "\n\nLe prix de la <:gem_{0}:{1}>`{0}` dépend du plafond du compte".format("bank_upgrade", GF.get_idmoji("bank_upgrade"))
-#
-#             msg.add_field(name="Commandes", value=desc, inline=False)
-#             await ctx.channel.send(embed = msg)
-#             sql.updateComTime(ID, "bank_bal", "gems")
-#             return
-#         else:
-#             msg = "Il faut attendre "+str(GF.couldown_4s)+" secondes entre chaque commande !"
-#         await ctx.channel.send(msg)
-#         return
-#     #=======================================================================
-#     # Ajoute ou enlève des Gems sur le compte épargne
-#     # un nombre positif ajoute des Gems
-#     # un nombre négatif enlève des Gems
-#     #=======================================================================
-#     elif mARG == "add":
-#         if sql.spam(ID,GF.couldown_4s, "bank_add", "gems"):
-#             if ARG2 != None:
-#                 ARG2 = int(ARG2)
-#                 gems = sql.valueAtNumber(ID, "gems", "gems")
-#                 solde = sql.valueAtNumber(ID, "Solde", "bank")
-#                 soldeMax = sql.valueAtNumber(ID, "SoldeMax", "bank")
-#                 if soldeMax == 0:
-#                     soldeMax = Taille
-#
-#                 if ARG2 <= gems:
-#                     soldeNew = solde + ARG2
-#                     if soldeNew > soldeMax:
-#                         ARG2 = ARG2 - (soldeNew - soldeMax)
-#                         msg = "Plafond de {} :gem:`gems` du compte épargne atteint\n".format(soldeMax)
-#                     elif soldeNew < 0:
-#                         msg = "Le solde de ton compte épargne ne peux être négatif.\nSolde du compte: {} :gem:`gems`".format(solde)
-#                         await ctx.channel.send(msg)
-#                         return
-#                     nbgm = -1*ARG2
-#                     sql.addGems(ID, nbgm)
-#                     sql.add(ID, "solde", ARG2, "bank")
-#                     msg += "Ton compte épargne a été crédité de {} :gem:`gems`".format(ARG2)
-#                     msg += "\nNouveau solde: {} :gem:`gems`".format(sql.valueAtNumber(ID, "Solde", "bank"))
-#                     sql.updateComTime(ID, "bank_add", "gems")
-#                 else:
-#                     msg = "Tu n'as pas assez de :gem:`gems` pour épargner cette somme"
-#             else:
-#                 msg = "Il manque le nombre de :gem:`gems` à ajouter sur votre compte épargne"
-#         else:
-#             msg = "Il faut attendre "+str(GF.couldown_4s)+" secondes entre chaque commande !"
-#         await ctx.channel.send(msg)
-#         return
-#     #=======================================================================
-#     # Fonction d'épargne
-#     # L'intéret est de 20% avec un bonus de 1% pour chanque bank_upgrade possédée
-#     #=======================================================================
-#     elif mARG == "saving":
-#         if sql.spam(ID,GF.couldown_4h, "bank_saving", "gems"):
-#             solde = sql.valueAtNumber(ID, "Solde", "bank")
-#             soldeMax = sql.valueAtNumber(ID, "SoldeMax", "bank")
-#             if soldeMax == 0:
-#                 soldeMax = Taille
-#             soldeMult = soldeMax/Taille
-#             pourcentage = 0.150 + soldeMult*0.002
-#             if pourcentage > 0.6:
-#                 pourcentage = 0.6
-#             soldeAdd = pourcentage*solde
-#             soldeTaxe = GF.taxe(soldeAdd, 0.1)
-#             soldeAdd = soldeTaxe[1]
-#             sql.add(ID, "solde", int(soldeAdd), "bank")
-#             msg = "Tu as épargné {} :gem:`gems`\n".format(int(soldeAdd))
-#             soldeNew = solde + soldeAdd
-#             if soldeNew > soldeMax:
-#                 soldeMove = soldeNew - soldeMax
-#                 nbgm = -1 * soldeMove
-#                 sql.addGems(ID, int(soldeMove))
-#                 sql.add(ID, "solde", int(nbgm), "bank")
-#                 msg += "Plafond de {} :gem:`gems` du compte épargne atteint\nTon épargne a été tranférée sur ton compte principal\n\n".format(soldeMax)
-#             msg += "Nouveau solde: {} :gem:`gems`".format(sql.valueAtNumber(ID, "Solde", "bank"))
-#
-#             D = r.randint(0,20)
-#             if D == 20 or D == 0:
-#                 sql.add(ID, "lootbox_raregems", 1, "inventory")
-#                 msg += "\nTu as trouvé une **Loot Box Gems Rare**! Utilise la commande `boxes open raregems` pour l'ouvrir"
-#             elif D >= 9 and D <= 11:
-#                 sql.add(ID, "lootbox_commongems", 1, "inventory")
-#                 msg += "\nTu as trouvé une **Loot Box Gems Common**! Utilise la commande `boxes open commongems` pour l'ouvrir"
-#             elif (jour.month == 12 and jour.day >= 22) and (jour.month == 12 and jour.day <= 25):
-#                 nbgift = 1
-#                 sql.add(ID, "lootbox_gift", nbgift, "inventory")
-#                 msg += "\n\nTu as trouvé {} :gift:`cadeau de Noël (gift)`".format(nbgift)
-#             elif (jour.month == 12 and jour.day >= 30) or (jour.month == 1 and jour.day <= 2):
-#                 nbgift = 1
-#                 sql.add(ID, "lootbox_gift", nbgift, "inventory")
-#                 msg += "\n\nTu as trouvé {} :gift:`cadeau de la nouvelle année (gift)`:confetti_ball:".format(nbgift)
-#
-#             sql.addGems(wel.idBaBot,int(soldeTaxe[0]))
-#             sql.updateComTime(ID, "bank_saving", "gems")
-#             lvl.addxp(ID, 4, "gems")
-#         else:
-#             ComTime = sql.valueAtNumber(ID, "bank_saving", "gems_com_time")
-#             time = float(ComTime) - (t.time()-GF.couldown_4h)
-#             timeH = int(time / 60 / 60)
-#             time = time - timeH * 3600
-#             timeM = int(time / 60)
-#             timeS = int(time - timeM * 60)
-#             msg = "Il te faut attendre :clock2:`{}h {}m {}s` avant d'épargner à nouveau !".format(timeH,timeM,timeS)
-#         await ctx.channel.send(msg)
-#         return
+def bank(param):
+    """Compte épargne"""
+    # =======================================================================
+    # Initialistation des variables générales de la fonction
+    # =======================================================================
+    ID = sql.get_SuperID(param["ID"], param["name_pl"])
+    ARG = param["ARG"]
+    ARG2 = param["ARG2"]
+    if ID == "Error 404":
+        return GF.WarningMsg[1]
+    PlayerID = sql.get_PlayerID(ID, "gems")
+    msg = []
+
+    jour = dt.date.today()
+    if ARG != "None":
+        mARG = ARG.lower()
+    else:
+        mARG = "bal"
+    for c in GF.objetOutil:
+        if c.type == "bank":
+            Taille = c.poids
+    desc = ""
+    solde = sql.valueAt(PlayerID, "Solde", "bank")
+    if solde == 0:
+        sql.add(PlayerID, "SoldeMax", Taille, "bank")
+    # =======================================================================
+    # Affiche le menu principal de la banque
+    # !bank bal <nom d'un joueur> permet de visualiser l'état de la banque de ce joueur
+    # =======================================================================
+    if mARG == "bal":
+        if sql.spam(PlayerID, GF.couldown_4s, "bank_bal", "gems"):
+            msg.append("bal")
+            if ARG2 != "None":
+                ID = sql.get_SuperID(sql.nom_ID(param["ARG2"]), param["name_pl"])
+                PlayerID = sql.get_PlayerID(ID, "gems")
+            solde = sql.valueAtNumber(PlayerID, "Solde", "bank")
+            soldeMax = sql.valueAtNumber(PlayerID, "SoldeMax", "bank")
+            if soldeMax == 0:
+                soldeMax = Taille
+            desc = "{} / {} :gem:`gems`\n".format(solde, soldeMax)
+            msg.append(desc)
+
+            desc = "bank **bal** *[name]* | Permet de connaitre la balance d'un utilisateur"
+            desc += "\nbank **add** *[+/- nombre]* | Permet d'ajouter ou d'enlever des :gem:`gems` de son compte épargne"
+            desc += "\nbank **saving** | Permet de calculer son épargne (utilisable toute les 4h)"
+            desc += "\n\nLe prix de la <:gem_{0}:{1}>`{0}` dépend du plafond du compte".format("bank_upgrade", "{idmoji[gem_bank_upgrade]}")
+
+            msg.append(desc)
+            sql.updateComTime(PlayerID, "bank_bal", "gems")
+        else:
+            desc = "Il faut attendre "+str(GF.couldown_4s)+" secondes entre chaque commande !"
+            msg.append("couldown")
+            msg.append(desc)
+        return msg
+    # =======================================================================
+    # Ajoute ou enlève des Gems sur le compte épargne
+    # un nombre positif ajoute des Gems
+    # un nombre négatif enlève des Gems
+    # =======================================================================
+    elif mARG == "add":
+        if sql.spam(PlayerID, GF.couldown_4s, "bank_add", "gems"):
+            if ARG2 != None:
+                ARG2 = int(ARG2)
+                gems = sql.valueAtNumber(PlayerID, "gems", "gems")
+                solde = sql.valueAtNumber(PlayerID, "Solde", "bank")
+                soldeMax = sql.valueAtNumber(PlayerID, "SoldeMax", "bank")
+                if soldeMax == 0:
+                    soldeMax = Taille
+
+                if ARG2 <= gems:
+                    soldeNew = solde + ARG2
+                    if soldeNew > soldeMax:
+                        ARG2 = ARG2 - (soldeNew - soldeMax)
+                        desc = "Plafond de {} :gem:`gems` du compte épargne atteint\n".format(soldeMax)
+                    elif soldeNew < 0:
+                        desc = "Le solde de ton compte épargne ne peux être négatif.\nSolde du compte: {} :gem:`gems`".format(solde)
+                        msg.append("NOK")
+                        msg.append(desc)
+                        return msg
+                    nbgm = -1*ARG2
+                    sql.addGems(PlayerID, nbgm)
+                    sql.add(PlayerID, "solde", ARG2, "bank")
+                    desc += "Ton compte épargne a été crédité de {} :gem:`gems`".format(ARG2)
+                    desc += "\nNouveau solde: {} :gem:`gems`".format(sql.valueAtNumber(PlayerID, "Solde", "bank"))
+                    msg.append("add")
+                    sql.updateComTime(PlayerID, "bank_add", "gems")
+                else:
+                    desc = "Tu n'as pas assez de :gem:`gems` pour épargner cette somme"
+                    msg.append("NOK")
+            else:
+                desc = "Il manque le nombre de :gem:`gems` à ajouter sur votre compte épargne"
+                msg.append("NOK")
+        else:
+            desc = "Il faut attendre "+str(GF.couldown_4s)+" secondes entre chaque commande !"
+            msg.append("couldown")
+        msg.append(desc)
+        return msg
+    # =======================================================================
+    # Fonction d'épargne
+    # L'intéret est de 20% avec un bonus de 1% pour chanque bank_upgrade possédée
+    # =======================================================================
+    elif mARG == "saving":
+        if sql.spam(PlayerID, GF.couldown_4h, "bank_saving", "gems"):
+            solde = sql.valueAtNumber(PlayerID, "Solde", "bank")
+            soldeMax = sql.valueAtNumber(PlayerID, "SoldeMax", "bank")
+            if soldeMax == 0:
+                soldeMax = Taille
+            soldeMult = soldeMax/Taille
+            pourcentage = 0.150 + soldeMult*0.002
+            if pourcentage > 0.6:
+                pourcentage = 0.6
+            soldeAdd = pourcentage*solde
+            soldeTaxe = GF.taxe(soldeAdd, 0.1)
+            soldeAdd = soldeTaxe[1]
+            sql.add(PlayerID, "solde", int(soldeAdd), "bank")
+            desc = "Tu as épargné {} :gem:`gems`\n".format(int(soldeAdd))
+            soldeNew = solde + soldeAdd
+            if soldeNew > soldeMax:
+                soldeMove = soldeNew - soldeMax
+                nbgm = -1 * soldeMove
+                sql.addGems(PlayerID, int(soldeMove))
+                sql.add(PlayerID, "solde", int(nbgm), "bank")
+                desc += "Plafond de {} :gem:`gems` du compte épargne atteint\nTon épargne a été tranférée sur ton compte principal\n\n".format(soldeMax)
+            desc += "Nouveau solde: {} :gem:`gems`".format(sql.valueAtNumber(PlayerID, "Solde", "bank"))
+
+            D = r.randint(0, 20)
+            if D == 20 or D == 0:
+                sql.add(PlayerID, "lootbox_raregems", 1, "inventory")
+                desc += "\nTu as trouvé une **Loot Box Gems Rare**! Utilise la commande `boxes open raregems` pour l'ouvrir"
+            elif D >= 9 and D <= 11:
+                sql.add(PlayerID, "lootbox_commongems", 1, "inventory")
+                desc += "\nTu as trouvé une **Loot Box Gems Common**! Utilise la commande `boxes open commongems` pour l'ouvrir"
+            elif (jour.month == 12 and jour.day >= 22) and (jour.month == 12 and jour.day <= 25):
+                nbgift = 1
+                sql.add(PlayerID, "lootbox_gift", nbgift, "inventory")
+                desc += "\n\nTu as trouvé {} :gift:`cadeau de Noël (gift)`".format(nbgift)
+            elif (jour.month == 12 and jour.day >= 30) or (jour.month == 1 and jour.day <= 2):
+                nbgift = 1
+                sql.add(PlayerID, "lootbox_gift", nbgift, "inventory")
+                desc += "\n\nTu as trouvé {} :gift:`cadeau de la nouvelle année (gift)`:confetti_ball:".format(nbgift)
+
+            try:
+                sql.addGems(sql.get_PlayerID(sql.get_SuperID(GF.idBaBot, "discord")), int(soldeTaxe[0]))
+            except:
+                print("Babot ne fait pas parti de la DB")
+            sql.updateComTime(PlayerID, "bank_saving", "gems")
+            lvl.addxp(PlayerID, 4, "gems")
+        else:
+            ComTime = sql.valueAtNumber(PlayerID, "bank_saving", "gems_com_time")
+            time = float(ComTime) - (t.time()-GF.couldown_4h)
+            timeH = int(time / 60 / 60)
+            time = time - timeH * 3600
+            timeM = int(time / 60)
+            timeS = int(time - timeM * 60)
+            desc = "Il te faut attendre :clock2:`{}h {}m {}s` avant d'épargner à nouveau !".format(timeH, timeM, timeS)
+        msg.append("saving")
+        msg.append(desc)
+        return msg
 
 
 def stealing(param):
