6306a4eb6a25ecb14c2f8efc599b26977f042cc3
Azerxim <mallet.maxime@hotmail.fr>
2020-02-03 13:20:26 +0100

    commandes admin pour manipuler la DB

---
+++ gems/gemsAdmin.py
@@ -0,0 +1,43 @@
+from DB import SQLite as sql
+from gems import gemsFonctions as GF
+
+
+def admin(param):
+    ID = sql.get_SuperID(param["ID"], param["name_pl"])
+    if ID == "Error 404":
+        return GF.WarningMsg[1]
+    PlayerID = sql.get_PlayerID(ID, "gems")
+    fct = param["fct"]
+    arg2 = param["arg2"]
+    arg3 = param["arg3"]
+    arg4 = param["arg4"]
+    msg = []
+
+    if fct == "init":
+        sql.init()
+    elif fct == "update":
+        # arg2 = nameDB | arg3 = fieldName | arg4 = fieldValue
+        desc = sql.updateField(PlayerID, arg3, arg4, arg2)
+    elif fct == "add":
+        # arg2 = nameDB | arg3 = nameElem | arg4 = nbElem
+        desc = sql.add(PlayerID, arg3, arg4, arg2)
+    elif fct == "value":
+        # arg2 = nameDB | arg3 = nameElem
+        desc = sql.valueAt(PlayerID, arg3, arg2)
+    elif fct == "gems":
+        # arg2 = nb gems
+        desc = sql.addGems(PlayerID, arg2)
+    elif fct == "spinelles":
+        # arg2 = nb spinelles
+        desc = sql.addSpinelles(PlayerID, arg2)
+    elif fct == "balance total" or fct == 'balancetotal':
+        desc = "Balance total"
+        desc += "\n{}:gem:".format(sql.countTotalGems())
+        spinelleidmoji = "{idmoji[spinelle]}"
+        desc += "\n{0}<:spinelle:{1}>".format(sql.countTotalSpinelles(), spinelleidmoji)
+    else:
+        desc = ":regional_indicator_s::regional_indicator_q::regional_indicator_l:"
+
+    msg.append("Admin {}".format(fct))
+    msg.append(str(desc))
+    return msg
