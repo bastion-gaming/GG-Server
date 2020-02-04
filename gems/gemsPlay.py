a8242801c579be6aaafb1aa0fe04f5a6d4a240b5
Azerxim <mallet.maxime@hotmail.fr>
2020-02-04 17:56:41 +0100

    commande ferment

--- gems/gemsPlay.py
+++ gems/gemsPlay.py
@@ -1263,3 +1263,124 @@ def hothouse(param):
         msg.append("couldown")
         msg.append(desc)
         return msg
+
+
+def ferment(param):
+    """**{grapes/wheat}** | Cave de fermentation. Alcool illimité !!"""
+    ID = sql.get_SuperID(param["ID"], param["name_pl"])
+    if ID == "Error 404":
+        return GF.WarningMsg[1]
+    PlayerID = sql.get_PlayerID(ID, "gems")
+    item = param["item"]
+    msg = []
+    desc = ""
+    Lang = sql.get_lang(param["IDGuild"])
+    gain = ""
+    i = 1
+    max = 20
+
+    if sql.spam(PlayerID, GF.couldown_4s, "ferment", "gems"):
+        if item == "grapes":
+            nbitem = 10
+            gain = "wine_glass"
+            couldown = GF.couldown_3h
+            couldownMsg = "3h"
+        elif item == "wheat":
+            nbitem = 8
+            gain = "beer"
+            couldown = GF.couldown_8h
+            couldownMsg = "8h"
+        sql.updateComTime(PlayerID, "ferment", "gems")
+        nbferment = sql.valueAtNumber(PlayerID, "barrel", "inventory") + 1
+        if nbferment >= max:
+            nbferment = max
+        msg.append("OK")
+        msg.append("{}".format(nbferment))
+        while i <= nbferment:
+            data = []
+            valueFerment = sql.valueAt(PlayerID, i, "ferment")
+            if valueFerment != 0:
+                valueTime = float(valueFerment[0])
+                valueItem = valueFerment[1]
+            else:
+                valueTime = 0
+                valueItem = ""
+            fermentItem = sql.valueAtNumber(PlayerID, item, "inventory")
+            if valueItem == "" and item == "None":
+                desc = "Ce barril est vide."
+            elif item == "grapes" or item == "wheat":
+                if valueTime == 0:
+                    if fermentItem >= nbitem:
+                        data = []
+                        data.append(str(t.time()))
+                        data.append(item)
+                        sql.add(PlayerID, i, data, "ferment")
+                        sql.add(PlayerID, item, -nbitem, "inventory")
+                        if item == "grapes":
+                            desc = "Ton barril a été rempli de :{0}:`{0}`. L'alcool aura fini de fermenter dans :clock2:`{1}`".format(item, couldownMsg)
+                        else:
+                            desc = "Ton barril a été rempli de <:gem_{0}:{1}>`{0}`. L'alcool aura fini de fermenter dans :clock2:`{2}`".format(item, "{idmoji[gem_" + item + "]}", couldownMsg)
+                    else:
+                        if item == "grapes":
+                            desc = "Tu n'as pas assez de :{0}:`{0}` dans ton inventaire! \nIl te faut {2} :{0}:`{0}` pour faire des :{1}:`{1}`".format(item, gain, nbitem)
+                        else:
+                            desc = "Tu n'as pas assez de <:gem_{0}:{1}>`{0}` dans ton inventaire! \nIl te faut {3} <:gem_{0}:{1}>`{0}` pour faire des :{2}:`{2}`".format(item, "{idmoji[gem_" + item + "]}", gain, nbitem)
+
+                else:
+                    if valueItem == "grapes":
+                        desc = "Fermentation de :{0}:`{0}` en cours.".format(valueItem)
+                    else:
+                        desc = "Fermentation de <:gem_{0}:{1}>`{0}` en cours.".format(valueItem, "{idmoji[gem_" + valueItem + "]}")
+            elif item == "None":
+                couldown = GF.couldown_3h
+                nbgain = 0
+                if valueItem == "grapes":
+                    gain = "wine_glass"
+                    nbgain = r.randint(1, 4)
+                    couldown = GF.couldown_3h
+                elif valueItem == "wheat":
+                    gain = "beer"
+                    nbgain = r.randint(2, 6)
+                    couldown = GF.couldown_8h
+                CookedTime = float(valueTime)
+                InstantTime = t.time()
+                time = CookedTime - (InstantTime-couldown)
+                if time <= 0:
+                    data = []
+                    data.append(0)
+                    data.append("")
+                    sql.add(PlayerID, gain, nbgain, "inventory")
+                    sql.updateField(PlayerID, i, data, "ferment")
+                    desc = "Ton alcool à fini de fermenter, en ouvrant le barril tu gagnes {2} :{0}:`{0}`".format(gain, "{idmoji[gem_" + gain + "]}", nbgain)
+                    lvl.addxp(PlayerID, 1, "gems")
+                    if i > 1:
+                        nbbarrel = int(sql.valueAtNumber(PlayerID, "barrel", "inventory"))
+                        if nbbarrel > 0:
+                            if sql.valueAtNumber(PlayerID, "barrel", "durability") == 0:
+                                for c in GF.objetOutil:
+                                    if c.nom == "barrel":
+                                        sql.add(PlayerID, "barrel", c.durabilite, "durability")
+                            sql.add(PlayerID, "barrel", -1, "durability")
+                            if sql.valueAtNumber(PlayerID, "barrel", "durability") <= 0:
+                                for c in GF.objetOutil:
+                                    if c.nom == "barrel":
+                                        sql.add(PlayerID, "barrel", c.durabilite, "durability")
+                                sql.add(PlayerID, "barrel", -1, "inventory")
+                else:
+                    timeH = int(time / 60 / 60)
+                    time = time - timeH * 3600
+                    timeM = int(time / 60)
+                    timeS = int(time - timeM * 60)
+                    if valueItem == "grapes":
+                        desc = "Fermentation de :{0}:`{0}` en cours.".format(valueItem)
+                    else:
+                        desc = "Fermentation de <:gem_{0}:{1}>`{0}` en cours.".format(valueItem, "{idmoji[gem_" + valueItem + "]}")
+                    desc += "\nTon alcool aura fini de fermenter dans :clock2:`{}h {}m {}s`".format(timeH, timeM, timeS)
+            msg.append("{}".format(i))
+            msg.append(desc)
+            i += 1
+    else:
+        desc = "Il faut attendre "+str(GF.couldown_4s)+" secondes entre chaque commande !"
+        msg.append("NOK")
+        msg.append(desc)
+    return msg
