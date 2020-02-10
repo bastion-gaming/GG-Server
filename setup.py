53f4fe8f74f55d2350c13a8ada9103db66a442f3
Azerxim <mallet.maxime@hotmail.fr>
2020-02-10 18:17:26 +0100

    amélioration visibilité

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 import subprocess
 import sys
 
-def install(package):
-    # try:
-    #     subprocess.call([sys.executable, "-m", "pip", "install", "-U", package, "--user"])
-    # except:
-    #     subprocess.call([sys.executable, "-m", "pip", "install", "-U", package])
-    subprocess.call([sys.executable, "-m", "pip", "install", "-U", package])
 
+def install(package):
+    try:
+        subprocess.call([sys.executable, "-m", "pip", "install", "-U", package, "--user"])
+    except:
+        subprocess.call([sys.executable, "-m", "pip", "install", "-U", package])
+    # subprocess.call([sys.executable, "-m", "pip", "install", "-U", package])
+
+
+# Base
 install("pip")
+
+# Base de données
 install("tinydb")
 install("pysqlite3")
+
+# Communication
 install("pyzmq")
 install("PyYAML")
+
+# Gestion des événements
 install("apscheduler")
