aa73b202c84cd00cadf0414c64bae2a9c78258ee
gnouf1 <jeuxforum26@gmail.com>
2020-03-14 02:13:16 +0100

    correction sur l'écriture du json

--- core/manage_seasons.py
+++ core/manage_seasons.py
@@ -58,7 +58,7 @@ def new_season(idS):
         # Ecriture de la nouvelle année dans le fichier
         dict_Dates[idS_old] = dict_Dates[idS_old][:6] + str(Dyear[idS_old]+1)
         path = "core/saisons.json"
-        with open(path, encoding='utf-8') as json_file:
+        with open(path, 'w', encoding='utf-8') as json_file:
             json.dump(dict_Dates, json_file, indent=4)
 
         # Reprogrammation de la nouvelle date
@@ -78,10 +78,10 @@ def init_season():
     # scheduler.add_job(new_season, 'date', run_date=date(Dyear[2], Dmonth[2], Dday[2]), args=[2], id="S2")
     # scheduler.add_job(new_season, 'date', run_date=date(Dyear[3], Dmonth[3], Dday[3]), args=[3], id="S3")
     # scheduler.add_job(new_season, 'date', run_date=date(Dyear[4], Dmonth[4], Dday[4]), args=[4], id="S4")
-    scheduler.add_job(new_season, 'date', run_date=datetime(Dyear[1], Dmonth[1], Dday[1], 1, 52, 5), args=[1], id="S1")
-    scheduler.add_job(new_season, 'date', run_date=datetime(Dyear[2], Dmonth[2], Dday[2], 1, 53, 5), args=[2], id="S2")
-    scheduler.add_job(new_season, 'date', run_date=datetime(Dyear[3], Dmonth[3], Dday[3], 1, 54, 5), args=[3], id="S3")
-    scheduler.add_job(new_season, 'date', run_date=datetime(Dyear[4], Dmonth[4], Dday[4], 1, 55, 5), args=[4], id="S4")
+    scheduler.add_job(new_season, 'date', run_date=datetime(Dyear[1], Dmonth[1], Dday[1], 2, 12, 5), args=[1], id="S1")
+    scheduler.add_job(new_season, 'date', run_date=datetime(Dyear[2], Dmonth[2], Dday[2], 2, 13, 5), args=[2], id="S2")
+    scheduler.add_job(new_season, 'date', run_date=datetime(Dyear[3], Dmonth[3], Dday[3], 2, 14, 5), args=[3], id="S3")
+    scheduler.add_job(new_season, 'date', run_date=datetime(Dyear[4], Dmonth[4], Dday[4], 2, 15, 5), args=[4], id="S4")
 
     scheduler.start()
 
